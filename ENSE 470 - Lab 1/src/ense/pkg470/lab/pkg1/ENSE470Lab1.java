8720f835b8a10c6fe42bd58912419d0ab4ea8e74
OriginalMidas <clayton.brez@gmail.com>
2016-01-26 14:56:07 -0600

    gg

--- ENSE 470 - Lab 1/src/ense/pkg470/lab/pkg1/ENSE470Lab1.java
+++ ENSE 470 - Lab 1/src/ense/pkg470/lab/pkg1/ENSE470Lab1.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class ENSE470Lab1 
 {
-    public static void main(String args) 
+    public static void main(String[] args[]) 
     {
         PersonalCustomer a = new PersonalCustomer("Clay", "HotTown", 9000);
         PersonalCustomer b = new PersonalCustomer("Colin", "GayTown", 9001);
